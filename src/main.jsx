4a10e26a2844d7a2cb1f6f3d40289145000f4aa3
Andrew Hudgins <andrewhudgins15@gmail.com>
2024-05-13 11:02:48 -0500

    Created the react browser router inside main.jsx + created individual pages in src/pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+import Error from "./pages/Error.jsx";
+import Landing from "./pages/Landing.jsx";
+import About from "./pages/About.jsx";
+import Testimonial from "./pages/Testimonial.jsx";
+import Contact from "./pages/Contact.jsx";
+import Services from "./pages/Services.jsx";
+import FAQ from "./pages/FAQ.jsx";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "testimonial",
+        element: <Testimonial />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+      {
+        path: "FAQ",
+        element: <FAQ />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
