64ea53a31f36a1a162d7221ec4881aa0072bb71d
Andrew Hudgins <andrewhudgins15@gmail.com>
2024-05-20 12:59:38 -0500

    Scrapped original hamburger and used only css + checkbox to create one

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,9 +1,16 @@
 function Hamburger({}) {
   return (
-    <div className="hamburgerContainer">
-      <span className="topHamburger"></span>
-      <span className="middleHamburger"></span>
-      <span className="bottomHamburger"></span>
+    <div>
+      <div className="hamburgerMenu"></div>
+      <label htmlFor="checkboxOne" className="hamburgerContainer">
+        <input type="checkbox" id="checkboxOne" className="hamburgerCheckbox" />
+        <div className="hamburger">
+          <span className="bar bar1"></span>
+          <span className="bar bar2"></span>
+          <span className="bar bar3"></span>
+          <span className="bar bar4"></span>
+        </div>
+      </label>
     </div>
   );
 }
