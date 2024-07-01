c77f295f59e6c430c149177de21e1eca518c0ac9
Andrew Hudgins <andrewhudgins15@gmail.com>
2024-07-01 12:56:00 -0500

    Added screen size breakpoint layout for pagebreak in contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,95 +1,120 @@
+import { useState, useEffect } from "react";
+
 import PageBreak from "../../components/PageBreak/PageBreak.jsx";
 import "./contact.css";
 
 function Contact({}) {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const breakpoint = 1200;
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <section className="contactPage">
-      <h2>Let's keep in touch!</h2>
-      <p>
-        Interested in connecting to learn more? I'd love to hear from you, and
-        would be honored to be apart of your journey! Feel free to use the form
-        below to start the conversation, send me an email directly, or reach out
-        via phone call / text.
-      </p>
+      <div>
+        <h2>Let's keep in touch!</h2>
+        <p>
+          Interested in connecting to learn more? I'd love to hear from you, and
+          would be honored to be apart of your journey! Feel free to use the
+          form below to start the conversation, send me an email directly, or
+          reach out via phone call / text.
+        </p>
 
-      <div className="info">
-        <a href="tel:979-900-6015">
-          <i className="fa-solid fa-phone fa-lg"></i>
-        </a>
-        <span id="myPhone">979.900.6015</span>
-      </div>
-      <div className="info">
-        <a href="mailto:katherine@tendertouchdoula.com" target="_blank">
-          <i className="fa-solid fa-envelope fa-lg"></i>
-        </a>
-        <span id="myEmail">katherine@tendertouchdoula.com</span>
-      </div>
-      <div className="info">
-        <a href="http://instagram.com/_u/katherinee.ellis/" target="_blank">
-          <i className="fa-brands fa-square-instagram fa-lg"></i>
-        </a>
-        <p>Don't forget to follow on Instagram!</p>
-      </div>
-
-      <PageBreak textColor={"var(--paleCrumble)"} />
-
-      <form name="contact" method="post">
-        <input type="hidden" name="form-name" value="contact"></input>
-
-        <div>
-          <div>
-            <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" name="firstName"></input>
-          </div>
-
-          <div>
-            <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" name="lastName"></input>
-          </div>
+        <div className="info">
+          <a href="tel:979-900-6015">
+            <i className="fa-solid fa-phone fa-lg"></i>
+          </a>
+          <span id="myPhone">979.900.6015</span>
         </div>
+        <div className="info">
+          <a href="mailto:katherine@tendertouchdoula.com" target="_blank">
+            <i className="fa-solid fa-envelope fa-lg"></i>
+          </a>
+          <span id="myEmail">katherine@tendertouchdoula.com</span>
+        </div>
+        <div className="info">
+          <a href="http://instagram.com/_u/katherinee.ellis/" target="_blank">
+            <i className="fa-brands fa-square-instagram fa-lg"></i>
+          </a>
+          <p>Don't forget to follow on Instagram!</p>
+        </div>
+      </div>
 
-        <label htmlFor="email">Email:</label>
-        <input type="text" id="email" name="email"></input>
+      <div>
+        {screenSize <= breakpoint ? (
+          <PageBreak textColor={"var(--paleCrumble)"} />
+        ) : (
+          ""
+        )}
 
-        <label htmlFor="phone">Phone:</label>
-        <input type="text" id="phone" name="phone"></input>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact"></input>
 
-        <label htmlFor="method">Preferred Contact Method:</label>
-        <select id="method" name="method">
-          <option value="" hidden>
-            Select an option
-          </option>
-          <option value="Email">Email</option>
-          <option value="Call">Phone Call</option>
-          <option value="Text">Text Message</option>
-        </select>
+          <div>
+            <div>
+              <label htmlFor="firstName">First Name:</label>
+              <input type="text" id="firstName" name="firstName"></input>
+            </div>
 
-        <label htmlFor="due">Expected Due Date:</label>
-        <input type="date" id="due" name="due"></input>
+            <div>
+              <label htmlFor="lastName">Last Name:</label>
+              <input type="text" id="lastName" name="lastName"></input>
+            </div>
+          </div>
 
-        <label htmlFor="location">Provider/Planned Birth Location:</label>
-        <input type="text" id="location" name="location"></input>
+          <label htmlFor="email">Email:</label>
+          <input type="text" id="email" name="email"></input>
 
-        <label htmlFor="Message">Message:</label>
-        <input type="textarea" id="Message" name="Message"></input>
+          <label htmlFor="phone">Phone:</label>
+          <input type="text" id="phone" name="phone"></input>
 
-        <label htmlFor="referral">How did you hear about me?</label>
-        <select id="referral" name="referral">
-          <option value="" hidden>
-            Select an option
-          </option>
-          <option value="BirthingCenter">Birthing Center</option>
-          <option value="Friend">Friend</option>
-          <option value="PreviousClient">Previous Client</option>
-          <option value="Google">Google</option>
-          <option value="Social">Social Media</option>
-          <option value="Other">Other</option>
-        </select>
+          <label htmlFor="method">Preferred Contact Method:</label>
+          <select id="method" name="method">
+            <option value="" hidden>
+              Select an option
+            </option>
+            <option value="Email">Email</option>
+            <option value="Call">Phone Call</option>
+            <option value="Text">Text Message</option>
+          </select>
 
-        <button type="submit" className="contactBtn">
-          Submit
-        </button>
-      </form>
+          <label htmlFor="due">Expected Due Date:</label>
+          <input type="date" id="due" name="due"></input>
+
+          <label htmlFor="location">Provider/Planned Birth Location:</label>
+          <input type="text" id="location" name="location"></input>
+
+          <label htmlFor="Message">Message:</label>
+          <input type="textarea" id="Message" name="Message"></input>
+
+          <label htmlFor="referral">How did you hear about me?</label>
+          <select id="referral" name="referral">
+            <option value="" hidden>
+              Select an option
+            </option>
+            <option value="BirthingCenter">Birthing Center</option>
+            <option value="Friend">Friend</option>
+            <option value="PreviousClient">Previous Client</option>
+            <option value="Google">Google</option>
+            <option value="Social">Social Media</option>
+            <option value="Other">Other</option>
+          </select>
+
+          <button type="submit" className="contactBtn">
+            Submit
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
